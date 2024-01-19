f9bb40629fd6b02ce6f274cb0f3dc7061efb40db
scrhartley <scrhartley@github.com>
2024-01-19 23:54:59 +0000

    Fix issue when using Spring lazy initialization

--- src/main/java/example/streaming/FreeMarkerConfig.java
+++ src/main/java/example/streaming/FreeMarkerConfig.java
@@ -2,13 +2,14 @@ package example.streaming;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import example.streaming.freemarker.custom.FlushBoundaryDirective;
 
 @Configuration
 public class FreeMarkerConfig {
 
-    @Bean
+    @Bean @Lazy(false)
     freemarker.template.Configuration freeMarkerTemplateConfiguration(
             org.springframework.web.servlet.view.freemarker.FreeMarkerConfig configurer) {
         freemarker.template.Configuration config = configurer.getConfiguration();
