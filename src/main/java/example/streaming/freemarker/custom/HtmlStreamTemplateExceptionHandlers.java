c30f32c605cb524d05fc64f86fc5be7143a2a281
scrhartley <scrhartley@github.com>
2024-04-12 23:54:41 +0100

    Fix indent

--- src/main/java/example/streaming/freemarker/custom/HtmlStreamTemplateExceptionHandlers.java
+++ src/main/java/example/streaming/freemarker/custom/HtmlStreamTemplateExceptionHandlers.java
@@ -70,8 +70,8 @@ public interface HtmlStreamTemplateExceptionHandlers {
                     // Clear page so there's only the error message.
                     pw.write(
                             "<script>(self => setTimeout(() =>\n" +
-                                    "document.body.innerHTML = self.previousElementSibling.outerHTML\n" +
-                                "))(document.currentScript);</script>");
+                                "document.body.innerHTML = self.previousElementSibling.outerHTML\n" +
+                            "))(document.currentScript);</script>");
                     //  Close the stream so that browser thinks it should render the debug HTML,
                     //  rather than just log the incomplete stream error in the console.
                     pw.close();
