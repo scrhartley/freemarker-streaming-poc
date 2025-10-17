0d5d8ab5e290013e35b263ad7d7666281edc91fd
scrhartley <scrhartley@github.com>
2025-10-17 18:15:32 +0100

    Improve handling of stuck futures

--- src/main/java/example/streaming/config/FreeMarkerConfig.java
+++ src/main/java/example/streaming/config/FreeMarkerConfig.java
@@ -1,10 +1,24 @@
 package example.streaming.config;
 
-import java.util.concurrent.ExecutorService;
+import static java.util.Collections.*;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import example.streaming.freemarker.custom.directive.DeferredHtmlDirective;
 import example.streaming.freemarker.custom.directive.FlushBoundaryDirective;
@@ -14,6 +28,8 @@ import example.streaming.util.future.LazyDirectExecutorService;
 
 @Configuration
 public class FreeMarkerConfig {
+
+    private static boolean CANCEL_UNCOMPLETED_FUTURES = true; // Intended to avoid leaving stuck threads.
 
     @Bean @Lazy(false)
     freemarker.template.Configuration freeMarkerTemplateConfiguration(
@@ -30,6 +46,48 @@ public class FreeMarkerConfig {
     @Bean
     ExecutorService mvcExecutorService() {
         return new LazyDirectExecutorService();
+    }
+
+
+    @Bean // Only needed so it can call Future.cancel
+    FreeMarkerViewResolver freeMarkerViewResolver(FreeMarkerProperties properties) {
+        class CustomFreeMarkerView extends FreeMarkerView {
+            @Override
+            public void render(@Nullable Map<String,?> model, HttpServletRequest request,
+                               HttpServletResponse response) throws Exception {
+                List<Future<?>> futures = CANCEL_UNCOMPLETED_FUTURES ? getFutures(model) : emptyList();
+                try {
+                    super.render(model, request, response);
+                } finally {
+                    for (Future<?> future : futures) {
+                        future.cancel(true);
+                    }
+                }
+            }
+            private List<Future<?>> getFutures(@Nullable Map<String, ?> model) {
+                return (model == null || model.isEmpty())
+                        ? emptyList()
+                        : model.values().stream()
+                                .filter(Future.class::isInstance)
+                                .<Future<?>>map(Future.class::cast)
+                                .collect(Collectors.toList());
+            }
+        }
+        class CustomFreeMarkerViewResolver extends FreeMarkerViewResolver {
+            @Override
+            protected Class<?> requiredViewClass() {
+                return CustomFreeMarkerView.class;
+            }
+            @Override
+            protected AbstractUrlBasedView instantiateView() {
+                return getViewClass() == CustomFreeMarkerView.class
+                        ? new CustomFreeMarkerView() : super.instantiateView();
+            }
+        }
+
+        FreeMarkerViewResolver resolver = new CustomFreeMarkerViewResolver();
+        properties.applyToMvcViewResolver(resolver);
+        return resolver;
     }
 
 }
