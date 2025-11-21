7fb937b3198737135353cebee61f5c63858647ba
scrhartley <scrhartley@github.com>
2025-11-21 00:54:01 +0000

    Add async defer directives

--- src/main/java/example/streaming/config/FreeMarkerConfig.java
+++ src/main/java/example/streaming/config/FreeMarkerConfig.java
@@ -4,9 +4,11 @@ import static java.util.Collections.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +22,9 @@ import org.springframework.web.servlet.view.AbstractUrlBasedView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import example.streaming.freemarker.custom.TrackedModelFutures;
+import example.streaming.freemarker.custom.directive.AsyncDeferHtmlDirective;
+import example.streaming.freemarker.custom.directive.AsyncRenderDeferredHtmlDirective;
 import example.streaming.freemarker.custom.directive.DeferHtmlDirective;
 import example.streaming.freemarker.custom.directive.FlushBoundaryDirective;
 import example.streaming.freemarker.custom.directive.RenderDeferredHtmlDirective;
@@ -38,6 +43,8 @@ public class FreeMarkerConfig {
         config.setSharedVariable("atom", new FlushBoundaryDirective());
         config.setSharedVariable("defer", new DeferHtmlDirective());
         config.setSharedVariable("renderDeferred", new RenderDeferredHtmlDirective());
+        config.setSharedVariable("asyncDefer", new AsyncDeferHtmlDirective());
+        config.setSharedVariable("renderAsyncDeferred", new AsyncRenderDeferredHtmlDirective());
         config.setSharedVariable("disallowAutoStreaming", Streaming.disallowAutoStreamingDirective());
         config.setSharedVariable("isAutoStreamingAllowed", Streaming.checkAutoStreamingAllowedModel());
         return config;
@@ -49,7 +56,7 @@ public class FreeMarkerConfig {
     }
 
 
-    @Bean // Only needed so it can call Future.cancel
+    @Bean // Originally added so it can call Future.cancel
     FreeMarkerViewResolver freeMarkerViewResolver(FreeMarkerProperties properties) {
         class CustomFreeMarkerView extends FreeMarkerView {
             @Override
@@ -57,6 +64,7 @@ public class FreeMarkerConfig {
                                HttpServletResponse response) throws Exception {
                 List<Future<?>> futures = CANCEL_UNCOMPLETED_FUTURES ? getFutures(model) : emptyList();
                 try {
+                    trackCompletableFutures(model, request);
                     super.render(model, request, response);
                 } finally {
                     for (Future<?> future : futures) {
@@ -64,6 +72,7 @@ public class FreeMarkerConfig {
                     }
                 }
             }
+
             private List<Future<?>> getFutures(@Nullable Map<String, ?> model) {
                 return (model == null || model.isEmpty())
                         ? emptyList()
@@ -72,7 +81,25 @@ public class FreeMarkerConfig {
                                 .<Future<?>>map(Future.class::cast)
                                 .collect(Collectors.toList());
             }
+
+            private void trackCompletableFutures(@Nullable Map<String,?> model, HttpServletRequest request) {
+                TrackedModelFutures tracking = (TrackedModelFutures) request.getAttribute(TrackedModelFutures.KEY);
+                if (tracking == null) {
+                    tracking = new TrackedModelFutures();
+                    request.setAttribute(TrackedModelFutures.KEY, tracking);
+                }
+
+                if (model == null || model.isEmpty()) {
+                    return;
+                }
+                @SuppressWarnings("unchecked")
+                Stream<Map.Entry<String, CompletableFuture<Object>>> cfs = model.entrySet().stream()
+                        .filter(entry -> entry.getValue() instanceof CompletableFuture)
+                        .map(Map.Entry.class::cast);
+                tracking.addCompletableFutures(cfs);
+            }
         }
+
         class CustomFreeMarkerViewResolver extends FreeMarkerViewResolver {
             @Override
             protected Class<?> requiredViewClass() {
--- src/main/java/example/streaming/freemarker/custom/directive/RenderDeferredHtmlDirective.java
+++ src/main/java/example/streaming/freemarker/custom/directive/RenderDeferredHtmlDirective.java
@@ -31,10 +31,10 @@ public class RenderDeferredHtmlDirective implements TemplateDirectiveModel {
         if (body != null) {
             throw new TemplateModelException("This directive doesn't allow a body");
         }
-        trigger(env);
+        execute(env);
     }
 
-    private static void trigger(Environment env) throws TemplateException, IOException {
+    protected void execute(Environment env) throws TemplateException, IOException {
         Writer out = env.getOut();
         LinkedHashMap<String, TemplateDirectiveBody> deferredMap = getAndClearPendingItems(env);
         while (deferredMap != null && !deferredMap.isEmpty()) {
@@ -42,27 +42,31 @@ public class RenderDeferredHtmlDirective implements TemplateDirectiveModel {
                 if (Streaming.isAutoStreamingAllowed(env)) {
                     out.flush(); // Rendering may block, so send buffered HTML to client first.
                 }
-
-                StringWriter writer = new StringWriter();
-                deferred.getValue().render(new ExceptionAwareWriter(writer, out));
-
-                StringBuilder builder = new StringBuilder();
-                builder.append("<template>").append(writer).append("</template>");
-                builder.append("<script>(() => {");
-                appendJavaScript(builder, deferred.getKey());
-                builder.append("})();</script>");
-
-                out.write(builder.toString());
+                render(deferred, out);
             }
             deferredMap = getAndClearPendingItems(env); // May be new ones due to nesting
         }
+    }
+
+    protected void render(
+            Map.Entry<String, TemplateDirectiveBody> deferred, Writer out) throws TemplateException, IOException {
+        StringWriter writer = new StringWriter();
+        deferred.getValue().render(new ExceptionAwareWriter(writer, out));
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("<template>").append(writer).append("</template>");
+        builder.append("<script>(() => {");
+        appendJavaScript(builder, deferred.getKey());
+        builder.append("})();</script>");
+
+        out.write(builder.toString());
     }
 
     // Replace the fallback with the real content.
     // Expect fallback to look something like:
     // <!--FMD$--><template id="fbId"></template><div>1</div><div>2</div><!--/FMD$-->
     // (with the template always empty and the fallback's nodes following it)
-    private static void appendJavaScript(StringBuilder builder, String fallbackId) {
+    private void appendJavaScript(StringBuilder builder, String fallbackId) {
         builder.append("const self = document.currentScript;");
         builder.append("const contentNode = self.previousSibling;"); // template
         builder.append("contentNode.remove();"); // Detach from DOM tree, but keep a reference.
@@ -74,7 +78,7 @@ public class RenderDeferredHtmlDirective implements TemplateDirectiveModel {
         builder.append("let node = fb.previousSibling;"); // Start from opening comment
         builder.append("do {");
         builder.append(    "if (node.nodeType === ").append(COMMENT_NODE);
-        builder.append(            " && node.data === '").append(END_DATA).append("') {");
+        builder.append(            " && node.data === '").append(getEndDataMarker()).append("') {");
         builder.append(        "break;");
         builder.append(    "}");
         builder.append(    "const nextNode = node.nextSibling;");
@@ -85,6 +89,10 @@ public class RenderDeferredHtmlDirective implements TemplateDirectiveModel {
 
         builder.append("fbParent.replaceChild(contentNode.content, node);"); // Replace end comment
         builder.append("self.remove();");
+    }
+
+    protected String getEndDataMarker() {
+        return DeferHtmlDirective.END_DATA;
     }
 
 }
